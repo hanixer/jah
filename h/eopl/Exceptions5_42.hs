1cb9b0c1e953720fcd72b5d9bb0fd370e2e7dfb0
hanixer <keil@thebest-best.narod.ru>
2017-02-13 00:21:46 +0200

    Exceptions

---
+++ h/eopl/Exceptions5_42.hs
@@ -0,0 +1,590 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+module Exceptions where 
+
+import Control.Monad.Except
+import Control.Monad.Trans.Except (throwE)
+import Control.Monad.Writer
+import Parser 
+import Control.Applicative ((<|>), many)
+import System.IO
+import qualified Data.IntMap as IM
+import qualified Data.List as L
+import Control.Monad.State
+
+-- Implicit references - variables can store only references to locations with values, but not values
+-- Added support for exceptions
+-- After raising an exception execution continues from point it was aborted by "raise"
+
+type Name = String
+
+-- AST             
+newtype Program = Program Exp
+                deriving (Show)
+data Exp = NumExp Int
+          | IsZeroExp Exp
+          | IfExp Exp Exp Exp
+          | VarExp String
+          | LetExp String Exp Exp
+          | ProcExp [String] Exp
+          | LetRecExp String [String] Exp Exp
+          | CallExp Exp [Exp]
+          | CompoundExp [Exp]
+          -- Arithmetic
+          | DiffExp Exp Exp
+          | MinusExp Exp
+          | AddExp Exp Exp
+          | MultExp Exp Exp
+          | ModExp Exp Exp
+          | EqExp Exp Exp
+          | GreaterExp Exp Exp
+          | LessExp Exp Exp
+          | DivExp Exp Exp
+          -- List operations
+          | ConsExp Exp Exp
+          | CarExp Exp
+          | CdrExp Exp
+          | IsEmptyExp Exp
+          | EmptyExp
+          | ListExp [Exp]
+          | CondExp [(Exp, Exp)]
+          -- Storage
+          | NewRefExp Exp
+          | SetRefExp Exp Exp
+          | DerefExp Exp
+          | AssignExp String Exp
+          -- Exceptions
+          | TryExp Exp String Exp
+          | RaiseExp Exp
+          | LetccExp String Exp
+          -- Other
+          | PrintExp Exp
+          deriving (Show)
+
+numExp :: Parser Exp
+numExp = do n <- number
+            return (NumExp n)
+
+binOp :: String -> (Exp -> Exp -> Exp) -> Parser Exp
+binOp = binaryOp expr
+
+unaryOp :: String -> (Exp -> Exp) -> Parser Exp
+unaryOp name combine = do _ <- strTok name
+                          _ <- strTok "("
+                          e <- expr
+                          _ <- strTok ")"
+                          return (combine e)
+
+diffExp :: Parser Exp
+diffExp = binOp "-" DiffExp
+
+isZeroExp :: Parser Exp
+isZeroExp = do space
+               _ <- strTok "zero?"
+               space
+               _ <-char '('
+               e <- expr
+               _ <- char ')'
+               return (IsZeroExp e)
+
+ifExp :: Parser Exp
+ifExp = do _ <- strTok "if"
+           e1 <- expr
+           _ <- strTok "then"
+           e2 <- expr
+           _ <- strTok "else"
+           e3 <- expr
+           return (IfExp e1 e2 e3)
+
+condPart :: Parser (Exp, Exp)
+condPart = do e1 <- expr
+              _ <- strTok "==>"
+              e2 <- expr
+              return (e1, e2)
+
+condExp :: Parser Exp
+condExp = do _ <- strTok "cond"
+             parts <- many condPart
+             _ <- strTok "end"
+             return (CondExp parts)
+
+varExp :: Parser Exp
+varExp = do v <- identifier
+            return (VarExp v)
+
+letExp :: Parser Exp
+letExp = do _ <- strTok "let"
+            v <- identifier
+            _ <- strTok "="
+            e1 <- expr
+            _ <- strTok "in"
+            e2 <- expr
+            return (LetExp v e1 e2)
+
+varListExp :: Parser [String]
+varListExp = do 
+  v <- identifier
+  vs <- many (do 
+    _ <- strTok ","
+    varListExp)
+  return (v : concat vs)
+
+argsListExp :: Parser [String]
+argsListExp = do 
+  _ <- strTok "("
+  vs <- varListExp <|> return []
+  _ <- strTok ")"
+  return vs            
+
+procExp :: Parser Exp
+procExp = do 
+  _ <- strTok "proc"
+  args <- argsListExp
+  e <- expr
+  return (ProcExp args e)
+
+letrec :: Parser Exp
+letrec = do _ <- strTok "letrec"
+            name <- identifier
+            args <- argsListExp
+            _ <- strTok "="
+            pbody <- expr
+            _ <- strTok "in"
+            lbody <- expr
+            return (LetRecExp name args pbody lbody)
+
+callExp :: Parser Exp
+callExp = do _ <- strTok "("
+             e1 <- expr
+             es <- many expr
+             _ <- strTok ")"
+             return (CallExp e1 es)
+
+compoundExp :: Parser Exp
+compoundExp = do 
+  _ <- strTok "{" <|> strTok "begin"
+  e <- expr
+  es <- many
+          (do _ <- strTok ";"
+              expr)
+  _ <- strTok "}" <|> strTok "end"
+  return (CompoundExp (e : es))
+
+minusExp :: Parser Exp
+minusExp = unaryOp "minus" MinusExp
+
+emptyExp :: Parser Exp
+emptyExp = do _ <- strTok "empty"
+              return EmptyExp
+
+commaSeparExpList :: Parser [Exp]
+commaSeparExpList = 
+  (do e <- expr
+      (do _ <- strTok "," 
+          es <- commaSeparExpList
+          return (e:es)) 
+       <|> return [e]) 
+  <|> return []
+                   
+listExp :: Parser Exp
+listExp = do _ <- strTok "list"
+             _ <- strTok "("
+             es <- commaSeparExpList
+             _ <- strTok ")"
+             return (ListExp es)
+
+assignExp :: Parser Exp
+assignExp = do
+  _ <- strTok "set"
+  var <- identifier
+  _ <- strTok "="
+  e <- expr
+  return (AssignExp var e)
+
+tryExp :: Parser Exp
+tryExp = strTok "try" >> expr >>= \e1 -> strTok "catch" >> 
+  strTok "(" >> identifier >>= \v -> strTok ")" >> expr >>= \e2 -> 
+  return $ TryExp e1 v e2
+
+raiseExp :: Parser Exp
+raiseExp = strTok "raise" >> strTok "(" >> expr >>= \e -> strTok ")" >>
+ return (RaiseExp e)
+
+letccExp :: Parser Exp
+letccExp = strTok "letcc" >> identifier >>= \v -> strTok "in" >> 
+  expr >>= \e -> return $ LetccExp v e
+
+expr :: Parser Exp
+expr = numExp <|> letExp <|> ifExp <|> procExp <|> letrec <|> callExp <|>
+  compoundExp
+  <|> isZeroExp
+  <|> minusExp
+  <|> diffExp
+  <|> binOp "+" AddExp
+  <|> binOp "*" MultExp
+  <|> binOp "mod" ModExp
+  <|> binOp "equal?" EqExp
+  <|> binOp "less?" LessExp
+  <|> binOp "greater?" GreaterExp
+  <|> binOp "/" DivExp
+  <|> binOp "cons" ConsExp
+  <|> unaryOp "car" CarExp
+  <|> unaryOp "cdr" CdrExp
+  <|> unaryOp "empty?" IsEmptyExp
+  <|> emptyExp
+  <|> listExp
+  <|> condExp
+  <|> unaryOp "print" PrintExp
+  <|> unaryOp "newref" NewRefExp
+  <|> binOp "setref" SetRefExp
+  <|> unaryOp "deref" DerefExp <|>
+  assignExp <|>
+  tryExp <|>
+  raiseExp <|>
+  letccExp <|>
+  varExp
+
+program :: Parser Program
+program = do e <- expr
+             return (Program e)
+
+-- Expession values
+data ExpVal = IntVal Int
+            | BoolVal Bool
+            | ConsVal ExpVal ExpVal
+            | EmptyVal
+            | ProcVal [String] Exp Env
+            | RefVal Loc
+            | ContVal Cont
+
+instance Show ExpVal where
+  show (IntVal n) = "<" ++ show n ++ ">"
+  show (BoolVal b) = "<" ++ show b ++ ">"
+  show (RefVal loc) = "<loc:" ++ show loc ++ ">"
+  show (ProcVal vs _ _) = "<proc:" ++ show vs ++ ">"
+  show (ContVal _) = "<cont>"
+  show _ = "<otherval>"
+
+expValToNum :: ExpVal -> Int
+expValToNum (IntVal n) = n
+expValToNum _ = error "number expected"
+unboxInt :: ExpVal -> Int
+unboxInt = expValToNum
+
+expValToBool :: ExpVal -> Bool
+expValToBool (BoolVal v) = v
+expValToBool _ = error "bool expected"
+unboxBool :: ExpVal -> Bool
+unboxBool = expValToBool
+
+
+-- Environment
+type Env = [(Name, ExpVal)]
+
+emptyEnv :: Env
+emptyEnv = []
+
+extendEnv :: Name -> ExpVal -> Env -> Env
+extendEnv var val env = (var, val):env
+
+extendEnvMult :: [(Name, ExpVal)] -> Env -> Env
+extendEnvMult = (++)
+
+applyEnv :: Env -> Name -> Maybe ExpVal
+applyEnv env var = L.lookup var env
+
+-- Evaluation
+type InterpM a = StateT StoreData (WriterT [HistItem] (ExceptT ErrorData IO)) a
+
+eval :: Exp -> Env -> InterpM ExpVal
+eval (NumExp n) _ = return (IntVal n)
+eval (VarExp name) env = 
+  resolveVar name env
+eval (DiffExp e1 e2) env = do
+  val1 <- eval e1 env
+  val2 <- eval e2 env
+  return $ IntVal $ unboxInt val1 - unboxInt val2
+eval (MultExp e1 e2) env = do
+  val1 <- eval e1 env
+  val2 <- eval e2 env
+  return $ IntVal $ unboxInt val1 * unboxInt val2
+eval (LetExp var e1 e2) env = do 
+  rhs <- eval e1 env
+  loc <- newrefAndInit rhs
+  let newEnv = extendEnv var (RefVal loc) env
+  eval e2 newEnv
+eval (LetRecExp name args pbody body) env = do 
+  loc <- getNewLoc
+  let env' = extendEnv name (RefVal loc) env
+  let procVal = ProcVal args pbody env'
+  _ <- setLocVal loc procVal
+  eval body env'
+eval (ProcExp vars body) env = return $ ProcVal vars body env
+eval (CallExp rator rands) envOuter = do
+  randVals <- mapM (`eval` envOuter) rands
+  ratorVal <- eval rator envOuter
+  case ratorVal of
+    ProcVal vars body envInner -> do 
+      envInner' <- allocateArgs vars randVals envInner
+      eval body envInner'
+    d -> throwErr (ErrorData $ "proc val is expected but was " ++ show d)
+eval (NewRefExp _) _ = throwErr (ErrorData "newref is not available in this language")
+eval (SetRefExp _ _) _ = throwErr (ErrorData "setref is not available in this language")
+eval (DerefExp _) _ = throwErr (ErrorData "deref is not available in this language")
+eval (AssignExp var e) env = do
+  val <- eval e env
+  case applyEnv env var of
+    Just (RefVal loc) -> do
+      _ <- setLocVal loc val
+      return val
+    v -> throwErr (ErrorData ("RefVal is expected but got " ++ show v))
+eval (CompoundExp exps) env = do
+  vals <- mapM (`eval` env) exps
+  return (last vals)
+eval (IsZeroExp e) env = do 
+  val <- eval e env
+  if unboxInt val == 0 
+    then return (BoolVal True) 
+    else
+    return (BoolVal False)
+eval (IfExp e1 e2 e3) env = do
+  val1 <- eval e1 env
+  if unboxBool val1 
+  then eval e2 env
+  else eval e3 env
+eval val _ = error ("not available for " ++ show val)
+
+-- Continuations
+type Cont = (ExpVal -> InterpM ())
+type ECont = Cont -> Cont
+
+applyCont :: Cont -> ExpVal -> InterpM ()
+applyCont cont = cont
+
+endCont :: Cont
+endCont val = do
+  liftIO $ putStr "End of computation: "
+  liftIO $ print val
+
+zeroCont :: Cont -> Cont
+zeroCont cont val = applyCont cont $ BoolVal $ unboxInt val == 0
+
+letExpCont :: Name -> Exp -> Env -> Cont -> ECont -> Cont
+letExpCont var body env cont econt val =
+  evalk body (extendEnv var val env) cont econt
+
+ifTestCont :: Exp -> Exp -> Env -> Cont -> ECont -> Cont
+ifTestCont thenExp elseExp env cont econt val =
+  if unboxBool val
+    then evalk thenExp env cont econt
+    else evalk elseExp env cont econt
+
+operatorCont :: [Exp] -> Env -> Cont -> ECont -> Cont 
+operatorCont [randExp] env cont econt ratorVal = 
+  evalk randExp env (operandCont ratorVal cont econt) econt
+operatorCont _ _ _ _ _ = throwErr (ErrorData "procedure are only with one arg")
+
+operandCont :: ExpVal -> Cont -> ECont -> Cont
+operandCont (ProcVal [arg] body env) cont econt val =
+  evalk body (extendEnv arg val env) cont econt
+operandCont (ContVal cont) _ _ val =
+  cont val
+
+operandCont _ _ _ _ = throwErr (ErrorData "procedure are only with one arg")
+
+diffCont1 :: Exp -> Env -> Cont -> ECont -> Cont
+diffCont1 e2 env cont econt val1 = 
+  evalk e2 env (diffCont2 val1 cont) econt
+
+diffCont2 :: ExpVal -> Cont -> Cont
+diffCont2 val1 cont val2 = 
+  cont (IntVal $ unboxInt val1 - unboxInt val2)
+
+divCont1 :: Exp -> Env -> Cont -> ECont -> Cont
+divCont1 e2 env cont econt val1 = 
+  evalk e2 env (divCont2 val1 cont econt) econt
+
+divCont2 :: ExpVal -> Cont -> ECont -> Cont
+divCont2 val1 cont econt val2 = 
+  if unboxInt val2 == 0 
+    then econt cont (IntVal (-1))
+    else cont (IntVal $ unboxInt val1 `div` unboxInt val2)
+
+tryCatchCont :: String -> Exp -> Env -> ECont -> ECont
+tryCatchCont var catchExpr env econt cont val =
+  evalk catchExpr (extendEnv var val env) cont econt
+
+
+unhandledCont :: ECont
+unhandledCont _ _ = liftIO $ print "Unhandled exception"
+
+evalk :: Exp -> Env -> Cont -> ECont -> InterpM ()
+evalk (VarExp var) env cont _ = do
+  val <- resolveVar var env
+  cont val
+evalk (NumExp n) _ cont _ = 
+  cont $ IntVal n
+evalk (ProcExp vars body) env cont _ =
+  cont $ ProcVal vars body env
+evalk (IsZeroExp e) env cont econt = 
+  evalk e env (zeroCont cont) econt
+evalk (IfExp e1 e2 e3) env cont econt =
+  evalk e1 env (ifTestCont e2 e3 env cont econt) econt
+evalk (LetExp name rhs body) env cont econt =
+  evalk rhs env (letExpCont name body env cont econt) econt
+evalk (DiffExp e1 e2) env cont econt =
+  evalk e1 env (diffCont1 e2 env cont econt) econt
+evalk (DivExp e1 e2) env cont econt =
+  evalk e1 env (divCont1 e2 env cont econt) econt
+evalk (CallExp e1 es2) env cont econt =
+  evalk e1 env (operatorCont es2 env cont econt) econt
+evalk (TryExp e1 v e2) env cont econt = 
+  evalk e1 env cont (tryCatchCont v e2 env econt)
+evalk (RaiseExp e) env cont econt = 
+  evalk e env (econt cont) econt
+evalk (LetccExp v body) env cont econt = 
+  evalk body (extendEnv v (ContVal cont) env) cont econt
+evalk _ _ _ _ = return ()
+
+resolveVar :: Name -> Env -> InterpM ExpVal
+resolveVar var env = 
+  case applyEnv env var of
+    Just (RefVal loc) -> do
+      mval <- deref loc
+      case mval of
+        Just val -> return val
+        Nothing ->
+          throwErr (ErrorData "location not found")
+    Just val -> return val
+    _ -> throwErr (ErrorData $ "variable " ++ var ++ "not found   "  ++ show env)
+
+allocateArgs :: [Name] -> [ExpVal] -> Env -> InterpM Env
+allocateArgs vars vals env = 
+  foldM (\e (var, val) -> do
+    loc <- newrefAndInit val
+    return (extendEnv var (RefVal loc) e)) env (zip vars vals)
+
+-- Errors
+data ErrorData = ErrorData String
+  deriving (Show)
+
+throwErr :: ErrorData -> InterpM a
+throwErr edat = lift $ lift $ throwE edat
+
+-- Storage
+data HistItem = NewRefItem Loc
+              | SetRefItem Loc ExpVal
+              | DerefItem Loc ExpVal
+
+instance Show HistItem where
+  show (NewRefItem n) = "[" ++ show n ++ "] new" 
+  show (SetRefItem n val) = "[" ++ show n ++ "] <- " ++ show val
+  show (DerefItem n val) = "[" ++ show n ++ "] => " ++ show val
+
+type Hist = [HistItem]
+type StoreData = (Int, IM.IntMap ExpVal)
+type Loc = Int
+
+getNewLoc :: InterpM Loc
+getNewLoc = do
+  (n, store) <- get
+  let n' = n + 1
+  put (n', store)
+  tell [NewRefItem n']
+  return n'
+
+setLocVal :: Loc -> ExpVal -> InterpM ExpVal
+setLocVal loc val = do
+  (n, store) <- get
+  put (n, IM.insert loc val store)
+  tell [SetRefItem loc val]
+  return val
+
+deref :: Int -> InterpM (Maybe ExpVal)
+deref loc = do
+  (n, store) <- get
+  case IM.lookup loc store of 
+    Just val -> do
+        put (n, store)
+        tell [DerefItem loc val]
+        return (Just val)
+    Nothing -> return Nothing
+
+newrefAndInit :: ExpVal -> InterpM Loc
+newrefAndInit val = do
+  loc <- getNewLoc
+  _ <- setLocVal loc val
+  return loc
+
+emptyState :: StoreData
+emptyState = (0 :: Int, IM.empty)
+
+-- Test helpers
+
+evall :: a
+evall = undefined
+
+evalFile :: FilePath -> IO ()
+evalFile file = do
+  handle <- openFile file ReadMode
+  _ <- hGetContents handle
+  return ()
+
+fileContents :: FilePath -> IO String
+fileContents file = do 
+  handle <- openFile file ReadMode
+  hGetContents handle
+
+-- showHistoryFile :: FilePath -> IO ()
+-- showHistoryFile file = do
+--   --contents <- fileContents file
+--   --let (_, (_, _, _)) = evall contents
+--   print "Storage:\n" 
+--   -- print stor
+--   -- mapM print
+--   --   (reverse hst)
+
+parseFile :: FilePath -> IO ()
+parseFile file = do
+  handle <- openFile file ReadMode
+  contents <- hGetContents handle
+  print contents
+  print $ fst $ head $ parse expr contents
+
+unwrapState
+  :: (Monoid w, Monad m) =>
+     StateT StoreData (WriterT w (ExceptT e m)) a -> m (Either e (a, w))
+unwrapState s = runExceptT $ runWriterT $ evalStateT s emptyState
+
+runk :: String -> IO ()
+runk str = do  
+  res <- unwrapState (evalk (fst $ head $ parse expr str) emptyEnv endCont unhandledCont)
+  case res of
+    Left (ErrorData errr) -> print errr
+    Right (_, h) -> do
+      mapM_ print h
+      return ()
+  
+run :: String -> IO ()
+run str = do  
+  res <- unwrapState (eval (fst $ head $ parse expr str) emptyEnv)
+  case res of
+    Left (ErrorData err) -> putStrLn ("Error: " ++ err)
+    Right (expres, _) ->
+      print expres
+
+runFile :: FilePath -> IO ()
+runFile file = do
+  contents <- fileContents file
+  run contents
+
+exs :: [String]
+exs = [
+  "let x = 200 in let f = proc (z) -(z,x) in let x = 100 in let g = proc (z) -(z,x) in -((f 1), (g 1))",
+  "let f = proc(x,y) -(x,*(y,-(x,2))) in (f 5 6)",
+  "letrec double(x) = if zero?(x) then 0 else -((double -(x,1)), -2) in (double 22)",
+  "let p = proc(x) set x = 4 in let a = 3 in {(p a);a}"
+  ]
+
+exl = last exs
+
+
--- trash/trampoline.cpp
+++ trash/trampoline.cpp
@@ -8,7 +8,7 @@ using namespace std;
 class Val
 {
 public:
-   virtual ~Val() {}
+    virtual ~Val() {}
 };
 
 typedef std::shared_ptr<Val> ValPtr;
@@ -19,8 +19,9 @@ typedef std::map<std::string, ValPtr> Env;
 class Cont
 {
 public:
-   virtual ~Cont() {}
-   virtual Bounce apply(ValPtr val) = 0;
+    virtual ~Cont() {}
+    virtual Bounce apply() = 0;
+    ValPtr val;
 };
 
 typedef std::shared_ptr<Cont> ContPtr;
@@ -28,135 +29,185 @@ typedef std::shared_ptr<Cont> ContPtr;
 class Exp
 {
 public:
-   virtual ~Exp() {}
-   virtual Bounce eval(const Env& env, ContPtr cont) = 0;
+    virtual ~Exp() {}
+    virtual Bounce eval(const Env& env, ContPtr cont) = 0;
 };
 typedef std::shared_ptr<Exp> ExpPtr;
 
 class Bounce
 {
 public:
-   Bounce(ValPtr val) : mVal(val) {}
-   Bounce(ContPtr cont) : mCont(cont) {}
+    Bounce(ValPtr val) : mVal(val) {}
+    Bounce(ContPtr cont) : mCont(cont) {}
 
-   bool hasCont() { return mCont.operator bool(); }
-   Bounce applyCont() { return mCont->apply(); }
+    bool hasCont() { return mCont.operator bool(); }
+    Bounce applyCont() { return mCont->apply(); }
 
 private:
-   ValPtr mVal;
-   ContPtr mCont;
+    ValPtr mVal;
+    ContPtr mCont;
 };
 
 class NumVal : public Val
 {
 public:
-   NumVal(int n) : num(n) {}
-   int num;
+    NumVal(int n) : num(n) {}
+    int num;
 };
 
 class NumExp : public Exp
 {
 public:
-   NumExp(int n) : mNum(n) {}
+    NumExp(int n) : mNum(n) {}
 
-   Bounce eval(const Env &, ContPtr ) override
-   {
-      return ValPtr(new NumVal{mNum});
-   }
+    Bounce eval(const Env &, ContPtr cont) override
+    {
+        cont->val = ValPtr(new NumVal{ mNum });
+        return Bounce(cont);
+    }
 
 private:
-   int mNum;
+    int mNum;
 };
 
 class VarExp : public Exp
 {
 public:
-   VarExp(const std::string& v) : mVar(v) {}
+    VarExp(const std::string& v) : mVar(v) {}
 
-   Bounce eval(const Env &env, ContPtr ) override
-   {
-      if (env.count(mVar) > 0)
-      {
-         ValPtr val = env.at(mVar);
-         return Bounce(val);
-      }
-      else
-      {
-         throw std::exception("Variable was not found");
-      }
-   }
+    Bounce eval(const Env &env, ContPtr cont) override
+    {
+        if (env.count(mVar) > 0)
+        {
+            cont->val = env.at(mVar);
+
+            return Bounce(cont);
+        }
+        else
+        {
+            throw std::exception("Variable was not found");
+        }
+    }
 private:
-   std::string mVar;
+    std::string mVar;
 };
 
 class ProcVal : public Val
 {
 public:
-   ProcVal(const std::string& s, ExpPtr b, Env e) : arg(s), body(b), env(e) {}
-   std::string arg;
-   ExpPtr body;
-   Env env;
+    ProcVal(const std::string& s, ExpPtr b, Env e) : arg(s), body(b), env(e) {}
+    std::string arg;
+    ExpPtr body;
+    Env env;
 };
 
 class ProcExp : public Exp
 {
 public:
-   ProcExp(const std::string& s, ExpPtr b) : arg(s), body(b) {}
+    ProcExp(const std::string& s, ExpPtr b) : arg(s), body(b) {}
 
-   Bounce eval(const Env &env, ContPtr ) override
-   {
-      return Bounce(ValPtr(new ProcVal{arg, body, env}));
-   }
+    Bounce eval(const Env &env, ContPtr cont) override
+    {
+        cont->val = ValPtr(new ProcVal{ arg, body, env });
+        return Bounce(cont);
+    }
 private:
-   std::string arg;
-   ExpPtr body;
+    std::string arg;
+    ExpPtr body;
 };
 
 class CallExp : public Exp
 {
 public:
-   CallExp(ExpPtr e1, ExpPtr e2) : mOperator(e1), mOperand(e2) {}
+    CallExp(ExpPtr e1, ExpPtr e2) : mOperator(e1), mOperand(e2) {}
 
-   Bounce eval(const Env &env, ContPtr cont) override
-   {
-      class MyCont : public Cont {
-         MyCont(const Env& env, ContPtr finalCont) : mEnv(env), mFinalCont(finalCont) {}
+    Bounce eval(const Env &env, ContPtr cont) override
+    {
+        class OperandCont : public Cont
+        {
+        public:
+            OperandCont(ContPtr finalCont, ValPtr procVal) : mFinalCont(finalCont), mProcVal(procVal) {}
 
-         Bounce apply() override
-         {
+            Bounce apply() override
+            {
+                if (ProcVal* procVal = dynamic_cast<ProcVal*>(mProcVal.get()))
+                {
+                    procVal->body;
+                    procVal->arg;
+                    Env env = procVal->env;
+                    env[procVal->arg] = val;
+                    return procVal->body->eval(env, mFinalCont);
+                }
+                else
+                {
+                    throw std::exception("Proc value is expected.");
+                }
+            }
+        private:
+            ContPtr mFinalCont;
+            ValPtr mProcVal;
+        };
 
-         }
-      private:
-         Env mEnv;
-         ContPtr mFinalCont;
-      };
+        class OperatorCont : public Cont {
+        public:
+            OperatorCont(const Env& env, ContPtr finalCont, ExpPtr operand) :
+                mEnv(env),
+                mFinalCont(finalCont),
+                mOperand(operand)
+            {}
 
-      return mOperator->eval(env, )
-   }
+            Bounce apply() override
+            {
+                return mOperand->eval(mEnv, ContPtr(new OperandCont(mFinalCont, val)));
+            }
+        private:
+            Env mEnv;
+            ContPtr mFinalCont;
+            ExpPtr mOperand;
+        };
+
+        return mOperator->eval(env, ContPtr(new OperatorCont(env, cont, mOperand)));
+    }
 
 private:
-   ExpPtr mOperator;
-   ExpPtr mOperand;
+    ExpPtr mOperator;
+    ExpPtr mOperand;
 };
 
 void trampoline(ExpPtr exp)
 {
-   Env env;
-   class EndCont : public Cont { Bounce apply() override { std::cout << "End."; return Bounce(ValPtr()); } };
-   ContPtr endCont(new EndCont);
-   Bounce bounce = exp->eval(env, endCont);
+    Env env;
+    class EndCont : public Cont
+    {
+        Bounce apply() override
+        {
+            if (NumVal* numv = dynamic_cast<NumVal*>(val.get()))
+            {
+                std::cout << "int is found: " << numv->num << "\n";
+            }
+            std::cout << "End."; return Bounce(ValPtr());
+        }
+    };
+    ContPtr endCont(new EndCont);
+    Bounce bounce = exp->eval(env, endCont);
 
-   while (bounce.hasCont())
-   {
-      bounce = bounce.applyCont();
-   }
+    while (bounce.hasCont())
+    {
+        bounce = bounce.applyCont();
+    }
 }
+
+ExpPtr ne(int n) { return ExpPtr(new NumExp(n)); }
+ExpPtr ve(std::string s) { return ExpPtr(new VarExp(s)); }
+ExpPtr pe(std::string s, ExpPtr b) { return ExpPtr(new ProcExp(s, b)); }
+ExpPtr ce(ExpPtr e1, ExpPtr e2) { return ExpPtr(new CallExp(e1, e2)); }
 
 int main()
 {
-   cout << "Hello World!" << endl;
-   ValPtr vvv(new ProcVal{"", 0, Env()});
+    cout << "Hello World!" << endl;
 
-   return 0;
+    // (((lambda (x) lambda (y) x) 5) 33) => 5
+    trampoline(ce(ce(pe("x",pe("y",ve("x"))),ne(5)),ne(33)));
+    return 0;
 }
 
