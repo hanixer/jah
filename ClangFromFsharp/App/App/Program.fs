c05be081decea70754e4194aef8e78578af4cde2
kaleb <kkaleb@imail.kz>
2017-09-01 16:44:20 +0300

    upload new

---
+++ ClangFromFsharp/App/App/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open ClangSharp
+
+[<EntryPoint>]
+let main argv = 
+    let visitit (child, parent) =
+        Cursor.ChildVisitResult.Break
+
+    use idx = new ClangSharp.Index()
+    let unit = idx.CreateTranslationUnit(@"D:\tmp\some.cpp")
+    
+    printfn "%A" argv
+    0 // return an integer exit code
